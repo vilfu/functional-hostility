db99e3efdfaa09eae5ef89c4ef121f2b4ae324f4
Ville Svärd <ville@funnel.io>
2019-12-23 15:27:02 +0100

    Now with pipes example

--- src/itrs.py
+++ src/itrs.py
@@ -1,4 +1,5 @@
 from itertools import chain, repeat, islice
+from pipe import take, chain_with
 
 import pytest
 
@@ -13,11 +14,17 @@ def iter_always_6_elements(lst):
     return islice(chain(lst, repeat(None)), 6)
 
 
+def pipe_always_6_elements(lst):
+    return (lst | chain_with(repeat(None))
+                | take(6))
+
 @pytest.mark.parametrize("lst, func", [
     (lst1, pythonic_always_6_elements),
     (lst1, iter_always_6_elements),
+    (lst1, pipe_always_6_elements),
 #    (range(4), pythonic_always_6_elements),
     (range(4), iter_always_6_elements),
+    (range(4), pipe_always_6_elements),
 ])
 def test_iter(lst, func):
     assert len(list(func(lst))) == 6
